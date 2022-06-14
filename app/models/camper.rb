7c8efcbc5b4460fd004934377767ca5f7a543637
Marcos Martinez <mmtz2108@gmail.com>
2022-06-14 10:46:31 -0500

    messed up range validation and could not get post signups to work.

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -3,5 +3,5 @@ class Camper < ApplicationRecord
     has_many :activities, through: :signups
 
     validates :name, presence: true
-    validates :age, numericality: true, exclusion: {in: [8..18]}
+    validates :age, numericality: {in: 8..18}
 end

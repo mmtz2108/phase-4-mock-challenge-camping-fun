2c876c43f934e2f456955d0873e047e71e9e3bcb
Marcos Martinez <mmtz2108@gmail.com>
2022-06-14 10:03:00 -0500

    added camper create method, activity index, rescue_from, specified routes

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,6 @@
 class ActivitiesController < ApplicationController
+    def index
+        activities = Activity.all
+        render json: activities
+    end
 end
--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,4 +1,7 @@
 class CampersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :no_record
+rescue_from ActiveRecord::RecordInvalid, with: :invalid
+
     def index
         campers = Camper.all
         render json: campers
@@ -7,5 +10,24 @@ class CampersController < ApplicationController
     def show
         camper = Camper.find(params[:id])
         render json: camper
+    end
+
+    def create
+        camper = Camper.create!(camper_params)
+        render json: camper
+    end
+
+    private
+
+    def no_record
+        render json: { error: 'Camper not found'}
+    end
+
+    def invalid
+        render json: { error: ['validation errors']}
+    end
+
+    def camper_params
+        params.permit(:name, :age)
     end
 end
